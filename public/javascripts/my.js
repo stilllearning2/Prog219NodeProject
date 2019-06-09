525c66094049f2db7490e51c6a9c9df4230b10fb
Lem1 <lemlemhaile2@gmail.com>
2019-06-09 15:45:10 -0700

    Update my.js

--- public/javascripts/my.js
+++ public/javascripts/my.js
@@ -105,9 +105,9 @@ function updatetranscript() {
     const updatedtranscript = { "Year": year, "Term": term, "Course": course, "Credits": credits, "Grade": grade };
 
     $.ajax({
-        url: '/updatedtranscript/' + oldCourse,  // putting the transcript Course in the URL for the PUT method
+        url: '/updatetranscript/' + oldCourse,  // putting the transcript Course in the URL for the PUT method
         //method: "PUT",
-        type:"PUT",
+        type:'PUT',
         dataType: 'json',
         contentType: 'application/json',
         data: JSON.stringify(updatedtranscript), 
